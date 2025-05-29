05a972a97c12649aa60dd0fc04cefe813ddfc21b
Bede-Ozy <bedeozy@gmail.com>
2025-05-29 22:33:47 +0100

    Completed: Full desktop UI

---
+++ app.js
@@ -0,0 +1,9 @@
+const allLinks = document.getElementsByTagName('a');
+
+console.log(allLinks);
+
+// Array.from(allLinks).forEach(a => {
+//     a.addEventListener('click', (e) => {
+//         e.preventDefault();
+//     })
+// });
